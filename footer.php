c1e0f0976748fd39b6dcc8a5410ae38fe2589cd3
Dylan <dylansprague11@hotmail.com>
2018-07-09 15:27:26 -0400

    Switched to Typed.js CDN

--- footer.php
+++ footer.php
@@ -13,14 +13,14 @@
 </footer>
 <!-- footer END -->
 
-<!-- jQuery,Popper.js,Bootstrap JS, Typed.js, main.js START -->
+<!-- jQuery,Popper.js,Bootstrap JS, Typed.js, baguetteBox.js main.js START -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js" async></script>
-<script src="js/typed.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.8/typed.min.js" async></script>
 <script src="js/main.js"></script>
-<!-- jQuery,Popper.js,Bootstrap JS, Typed.js, main.js END -->
+<!-- jQuery,Popper.js,Bootstrap JS, Typed.js, baguetteBox.js main.js END -->
 
 </body>
 </html>
