5fdcaf4610c2444e7573b99751afc782713f18f9
Pavillin <dylansprague11@hotmail.com>
2019-08-12 14:11:10 -0400

    added content to homepage

--- home.php
+++ home.php
@@ -10,6 +10,18 @@
     echo $buffer;
 ?>
 
-<p style="color: #fff; text-align: center; margin-top: 20px;">Soon&trade;</p>
-
+    <div class="home">
+        <div class="home-wrap">
+            <div class="col-md-8">
+                <div class="img mb-4" style="background-image: url(images/me.jpg);"></div>
+                <div class="desc">
+                    <hr>
+                    <h2 class="subheading">Hello I'm</h2>
+                    <h1 class="mb-4 heading">Dylan Sprague</h1>
+                    <p class="mb-4 bio">Hailing from Halifax, Nova Scotia, I'm a soon to be graduate of the Computer Programmer Diploma program at Georgian College! I'm a huge gamer and actively compete for <a href="https://twitter.com/frontside_gg" style="color: #ff9800">FrontSide Esports</a> on their Rocket League roster. When I'm not hitting the books or playing games you can catch me adventuring with my dog Oskar or spending time with friends. I'm an openminded individual with a huge love for everything tech and hope to be on the frontlines pushing the boundaries for what's possible with new technology!</p>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php include 'footer.php'; ?>
